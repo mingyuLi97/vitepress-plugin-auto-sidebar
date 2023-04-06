e3f512d7a12df911da45e89d81bee3a87ada43ef
limy <mingyuli1997@gmail.com>
2023-04-06 23:03:50 +0800

    initial

---
+++ test/.vitepress/config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vitepress";
+import VitePluginAutoSidebar from "vite-plugin-vitepress-auto-sidebar";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "demo",
+  description: "test plugin",
+  vite: {
+    plugins: [
+      VitePluginAutoSidebar({
+        sidebarResolved(value) {
+          console.log(value);
+        },
+        // ignores: ["index.md"],
+      }),
+    ],
+  },
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "dir1", link: "/dir1/dir1-1/1" },
+      { text: "dir2", link: "/dir2/2-2/2" },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
